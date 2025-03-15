af450da9eb9322232c2ac4b87d7b30978312cf52
JeroCaller <147837893+JeroCaller@users.noreply.github.com>
2025-03-15 18:55:04 +0900

    [Bronze V] Title: A×B, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw =
+            new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        String source = br.readLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(source);
+        Integer total = null;
+        while (stringTokenizer.hasMoreTokens()) {
+            int nextNum = Integer.parseInt(stringTokenizer.nextToken());
+            if (total == null) {
+                total = nextNum;
+                continue;
+            }
+            total *= nextNum;
+        }
+
+        bw.write(String.valueOf(total));
+
+        bw.flush();
+        bw.close();
+    }
+
+}
