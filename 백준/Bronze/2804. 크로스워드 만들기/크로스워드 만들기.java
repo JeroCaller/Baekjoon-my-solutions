5f713d5bcfd4c5ead1921157254b96036c681c6d
JeroCaller <147837893+JeroCaller@users.noreply.github.com>
2025-07-09 13:18:44 +0900

    [Bronze I] Title: 크로스워드 만들기, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/2804. 크로스워드 만들기/크로스워드 만들기.java
+++ 백준/Bronze/2804. 크로스워드 만들기/크로스워드 만들기.java
@@ -24,14 +24,13 @@ public class Main {
 
         // row, column
         int[] positions = new int[2];
-
-        outer: for (int i = 0; i < inputs[0].length(); ++i) {
-            for (int j = 0; j < inputs[1].length(); ++j) {
-                if (inputs[0].charAt(i) == inputs[1].charAt(j)) {
-                    positions[0] = j;
-                    positions[1] = i;
-                    break outer;
-                }
+        BinaryTree binaryTree = new BinaryTree(inputs[1]);
+        for (int i = 0; i < inputs[0].length(); ++i) {
+            BinaryTree.Node node = binaryTree.getTargetNode(inputs[0].charAt(i));
+            if (node != null) {
+                positions[0] = node.getPosition();
+                positions[1] = i;
+                break;
             }
         }
 
@@ -51,5 +50,91 @@ public class Main {
 
             bw.newLine();
         }
+    }
+}
+
+class BinaryTree {
+
+    class Node {
+        private char alphabet;
+        private int position;
+        private Node leftNode;
+        private Node rightNode;
+
+        public Node(char alphabet, int position) {
+            this.alphabet = alphabet;
+            this.position = position;
+        }
+
+        public char getAlphabet() {
+            return alphabet;
+        }
+
+        public int getPosition() {
+            return position;
+        }
+
+        public Node getLeftNode() {
+            return leftNode;
+        }
+
+        public void setLeftNode(Node newLeftNode) {
+            this.leftNode = newLeftNode;
+        }
+
+        public Node getRightNode() {
+            return rightNode;
+        }
+
+        public void setRightNode(Node newRightNode) {
+            this.rightNode = newRightNode;
+        }
+    }
+
+    private Node root = null;
+
+    public BinaryTree(String source) {
+        for (int i = 0; i < source.length(); ++i) {
+            insert(source.charAt(i), i);
+        }
+    }
+
+    public void insert(char alphabet, int pos) {
+        Node newNode = new Node(alphabet, pos);
+        root = _insert(root, newNode);
+    }
+
+    private Node _insert(Node targetNode, Node newNode) {
+        if (targetNode == null) {
+            return newNode;
+        }
+
+        if (targetNode.getAlphabet() >= newNode.getAlphabet()) {
+            targetNode.setLeftNode(_insert(targetNode.getLeftNode(), newNode));
+        } else {
+            targetNode.setRightNode(_insert(targetNode.getRightNode(), newNode));
+        }
+        return targetNode;
+    }
+
+    public Node getTargetNode(char targetChar) {
+        return search(root, targetChar);
+    }
+
+    private Node search(Node node, char targetChar) {
+        if (node == null) {
+            return null;
+        }
+
+        if (node.getAlphabet() == targetChar) {
+            return node;
+        }
+
+        if (node.getAlphabet() >= targetChar) {
+            node = search(node.getLeftNode(), targetChar);
+        } else {
+            node = search(node.getRightNode(), targetChar);
+        }
+        return node;
     }
 }
