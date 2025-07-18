32561e3e1c267b1c5f5f5e55bed7c65b95d791a4
JeroCaller <j69m17@gmail.com>
2025-07-18 13:25:44 +0900

    [Silver V] Title: 폴리오미노, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    private static final BufferedWriter bw =
+        new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final BufferedReader br =
+        new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        mySolution();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void mySolution() throws IOException {
+        String source = br.readLine();
+        String replaced = source.replace("XXXX", "AAAA")
+            .replace("XX", "BB");
+        if (replaced.contains("X")) {
+            bw.write("-1");
+        } else {
+            bw.write(replaced);
+        }
+    }
+
+}
