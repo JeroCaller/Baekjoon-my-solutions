a92ad93caf0460d57eb1bed4efb5fa870ad4b348
JeroCaller <j69m17@gmail.com>
2025-07-23 12:18:54 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    private static final BufferedWriter bw =
+        new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final BufferedReader br =
+        new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        mySolution();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void mySolution() throws IOException {
+        String source = br.readLine();
+        String target = br.readLine();
+        Matcher matcher = Pattern.compile(target).matcher(source);
+
+        int answer = 0;
+        while (matcher.find()) {
+            ++answer;
+        }
+
+        bw.write(String.valueOf(answer));
+    }
+}
