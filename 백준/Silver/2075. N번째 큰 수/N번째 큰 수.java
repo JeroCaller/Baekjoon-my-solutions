fe85ac0969dcc4ff9e47a7130c8177e2482e1b6f
JeroCaller <j69m17@gmail.com>
2025-10-19 15:10:25 +0900

    [Silver III] Title: N번째 큰 수, Time: 768 ms, Memory: 217292 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -3,7 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -23,17 +23,23 @@ public class Main {
 
     public static void mySolution() throws IOException {
         final int N = Integer.parseInt(br.readLine());
-        int[] numbers = new int[N * N];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(N);
 
         for (int i = 0; i < N; ++i) {
             StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
 
             for (int j = 0; j < N; ++j) {
-                numbers[i * N + j] = Integer.parseInt(stringTokenizer.nextToken());
+                int num = Integer.parseInt(stringTokenizer.nextToken());
+
+                if (pq.size() < N) {
+                    pq.offer(num);
+                } else if (pq.peek() < num) {
+                    pq.poll();
+                    pq.offer(num);
+                }
             }
         }
 
-        Arrays.sort(numbers);
-        bw.write(String.valueOf(numbers[N * N - N]));
+        bw.write(String.valueOf(pq.peek()));
     }
 }
