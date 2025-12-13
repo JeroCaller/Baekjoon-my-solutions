27e99255d3c3727be2d53aaae6c5b5986ebe9b18
JeroCaller <j69m17@gmail.com>
2025-12-13 10:09:23 +0900

    [Silver II] Title: 나무 자르기, Time: 984 ms, Memory: 119372 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final BufferedWriter bw =
+        new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final BufferedReader br =
+        new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        mySolution();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void mySolution() throws IOException {
+        String[] firstLine = br.readLine().split(" ");
+        final int N = Integer.parseInt(firstLine[0]);
+        final int M = Integer.parseInt(firstLine[1]);
+        int[] trees = new int[N];
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; ++i) {
+            trees[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        Arrays.sort(trees);
+        int result = findAnswer(trees, trees[trees.length - 1], M);
+        bw.write(String.valueOf(result));
+    }
+
+    private static boolean isCandidate(int[] trees, int m, int offer) {
+        long total = 0;
+
+        for (int i = 0; i < trees.length; ++i) {
+            if (trees[i] < offer) {
+                continue;
+            }
+
+            total += trees[i] - offer;
+        }
+
+        return total >= m;
+    }
+
+    private static int findAnswer(int[] trees, int maxTree, int m) {
+        int leftIdx = 0;
+        int rightIdx = maxTree;
+        int midIdx = (leftIdx + rightIdx) / 2;
+        int answer = 0;
+
+        while (leftIdx <= rightIdx) {
+            if (isCandidate(trees, m, midIdx)) {
+                answer = Math.max(answer, midIdx);
+                leftIdx = midIdx + 1;
+            } else {
+                rightIdx = midIdx - 1;
+            }
+
+            midIdx = (leftIdx + rightIdx) / 2;
+        }
+
+        return answer;
+    }
+}
