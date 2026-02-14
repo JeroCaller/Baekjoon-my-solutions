4b31749986cce1a1e306068e0d779e02272344e6
JeroCaller <j69m17@gmail.com>
2026-02-14 13:24:26 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1136 ms, Memory: 92032 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -3,8 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     private static final BufferedWriter bw =
@@ -12,7 +10,7 @@ public class Main {
     private static final BufferedReader br =
         new BufferedReader(new InputStreamReader(System.in));
 
-    private static final Map<String, Integer> dp = new HashMap<>();
+    private static final int[][][] memo = new int[21][21][21];
 
     public static void main(String[] args) throws IOException {
         mySolution();
@@ -23,55 +21,44 @@ public class Main {
     }
 
     public static void mySolution() throws IOException {
-        int[] abc = new int[3];
-
         while (true) {
             String[] inputs = br.readLine().split(" ");
+            int a = Integer.parseInt(inputs[0]);
+            int b = Integer.parseInt(inputs[1]);
+            int c = Integer.parseInt(inputs[2]);
 
-            for (int i = 0; i < 3; ++i) {
-                abc[i] = Integer.parseInt(inputs[i]);
-            }
+            if (a == -1 && b == -1 && c == -1) break;
 
-            if (abc[0] == -1 && abc[1] == -1 && abc[2] == -1) break;
-
-            int result = w(abc[0], abc[1], abc[2]);
-            bw.write(String.format("w(%d, %d, %d) = %d", abc[0], abc[1], abc[2], result));
+            int result = w(a, b, c);
+            bw.write(String.format("w(%d, %d, %d) = %d", a, b, c, result));
             bw.newLine();
         }
     }
 
     private static int w(int a, int b, int c) {
-        String key = makeIntoKey(a, b, c);
-        int result;
-
-        if (dp.containsKey(key)) {
-            return dp.get(key);
-        }
-
         if (a <= 0 || b <= 0 || c <= 0) {
-            dp.put(key, 1);
             return 1;
         }
 
         if (a > 20 || b > 20 || c > 20) {
-            result = w(20, 20, 20);
-            dp.put(key, result);
-            return result;
+            return w(20, 20, 20);
         }
+
+        if (memo[a][b][c] != 0) {
+            return memo[a][b][c];
+        }
+
+        int result;
 
         if (a < b && b < c) {
             result = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
-            dp.put(key, result);
+            memo[a][b][c] = result;
             return result;
         }
 
-        result = w(a - 1, b, c) + w(a - 1, b - 1, c)
-            + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
-        dp.put(key, result);
+        result = w(a - 1, b, c) + w(a - 1, b - 1, c) +
+            w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        memo[a][b][c] = result;
         return result;
-    }
-
-    private static String makeIntoKey(int a, int b, int c) {
-        return String.format("%d,%d,%d", a, b, c);
     }
 }
