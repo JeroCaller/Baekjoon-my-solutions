3ba169165addf6e8f04ebdd1e34aadc8caf1f0ea
JeroCaller <j69m17@gmail.com>
2026-03-12 11:41:11 +0900

    [Silver II] Title: 점프 점프, Time: 116 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.java
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -3,6 +3,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -31,25 +33,32 @@ public class Main {
             array[i] = Integer.parseInt(st.nextToken());
         }
 
-        final int[] memo = new int[n];
+        int answer = search();
 
-        for (int i = n - 2; i >= 0; --i) {
-            for (int j = 1; j <= array[i]; ++j) {
-                if (i + j > n - 1) break;
-                if (i + j != n - 1 && memo[i + j] == 0) continue;
+        if (answer == 0 && n != 1) {
+            bw.write("-1");
+        } else {
+            bw.write(String.valueOf(answer));
+        }
+    }
 
-                if (memo[i] == 0) {
-                    memo[i] = 1 + memo[i + j];
-                } else {
-                    memo[i] = Math.min(memo[i], 1 + memo[i + j]);
-                }
+    private static int search() {
+        final Queue<Integer> queue = new ArrayDeque<>();  // index
+        final int[] depth = new int[n];
+        queue.offer(0);
+
+        while (!queue.isEmpty() && depth[n - 1] == 0) {
+            int idx = queue.poll();
+
+            for (int i = 1; i <= array[idx]; ++i) {
+                if (i + idx > n - 1) break;
+                if (depth[i + idx] != 0) continue;
+
+                queue.offer(i + idx);
+                depth[i + idx] = depth[idx] + 1;
             }
         }
 
-        if (memo[0] == 0 && n != 1) {
-            bw.write("-1");
-        } else {
-            bw.write(String.valueOf(memo[0]));
-        }
+        return depth[n - 1];
     }
 }
